ff13df2b2b539553068c870518123ba1d6e162b8
Kevin Budd <kevin@kevinbudd.com>
2018-03-07 17:51:23 -0600

    add tests to api users controller

--- spec/controllers/api/users_controller_spec.rb
+++ spec/controllers/api/users_controller_spec.rb
@@ -1,5 +1,25 @@
 require 'rails_helper'
 
 RSpec.describe Api::UsersController, type: :controller do
+  let(:user) { create(:user) }
 
+  include AuthHelper
+  before(:each) do
+    http_login user
+  end
+
+  describe "GET index" do
+    it "returns https success" do
+      get :index
+      json = JSON.parse(response.body)
+      expect(response).to be_success
+    end
+
+    it "returns list of users" do
+      get :index
+      json = JSON.parse(response.body)
+      puts json[0]
+      expect(json[0]["email"]).to eq user.email
+    end
+  end
 end
